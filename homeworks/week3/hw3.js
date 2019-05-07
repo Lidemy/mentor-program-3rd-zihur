3f07dc96bb84604a9d1ae51b42b524da85ea96ed
Zihur <dog60508@gmail.com>
2019-05-07 22:36:17 +0800

    第三週作業繳交

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,3 +1,4 @@
+// 給定一個數字 n（1<=n<=100000），請回傳 n 是否為質數（質數的定義：除了 1 以外，所有小於他的數都無法整除）
 function isPrime(n) {
   const factor = [];
   for (let i = 1; i <= n; i += 1) {
--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,3 +1,4 @@
+// 給定一個長度小於 100 的字串 s，請回傳 s 是否為迴文（迴文的定義：正著唸倒著念都一樣）
 function isPalindromes(str) {
   let reverseStr = '';
   for (let i = str.length - 1; i >= 0; i -= 1) {
