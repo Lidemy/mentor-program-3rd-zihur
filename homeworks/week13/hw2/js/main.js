4167d8fbfb59239d55c11dd3dbc90064633e698d
kobbe <kobbe@yimaninfotek.net>
2019-09-06 20:53:18 +0800

    WIP week13 hw2

--- homeworks/week13/hw2/js/main.js
+++ homeworks/week13/hw2/js/main.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
   $("#button-add").click(function(){
-    const listText = `
-    <div class="input-group mb-3 px-3 flex-fill">
-      <div class="input-group-prepend">
-        <div class="input-group-text">
-          <input type="checkbox" aria-label="Checkbox for following text input">
+    if ($(".form-control").val() != "") {      
+      const listText = `
+      <div class="input-group mb-3 px-3">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <input type="checkbox" aria-label="Checkbox for following text input">
+          </div>
+        </div>
+        <div class="list__text text-secondary">
+          <p class="mb-0">${$(".form-control").val()}</p>
+        </div>
+        <div class="input-group-append list__close">
+          <button type="button" class="btn btn-danger close p-2" aria-label="Close">
+            <span aria-hidden="true" aria-label="Close">&times;</span>
+          </button>
         </div>
       </div>
-      <div class="list-item text-secondary border-top border-bottom px-3 mb-0 align-items-center d-flex">
-        <p class="mb-0">${$(".form-control").val()}</p>
-      </div>
-      <div class="input-group-append">
-        <button type="button" class="btn btn-danger" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-    </div>
-    `
-    $(".list__wrap").append($(".form-control").val(listText));
-    $(".form-control").val("");
+      `
+      $(".list").append(listText);
+      $(".form-control").val("");
+    }
   })
+
+  $(".list").click(function(e) {
+    // 刪除按鈕
+    if ($(e.target).attr("aria-label") === "Close") {
+      $(e.target).parentsUntil(".list").remove();
+    }
+
+    // 勾選完成按鈕
+    if ($(e.target).hasClass("list__checked")) {
+      $(e.target).parents(".input-group-text").toggleClass("completed");
+      $(e.target).parents(".input-group").find(".list__text").toggleClass("text-decoration-deleted");
+    };
+  })
+
+  $()  
 });
