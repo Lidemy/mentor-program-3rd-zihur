ca61f8f6b7572a6aae51e29b171f2bc93376957a
Zihur <dog60508@gmail.com>
2019-09-07 17:18:06 +0800

    Finish week13 hw2 & add setting to eslint
    作業二完成，同時在 ESlint 檔案裏面增加了 jQuery 的設定。

--- homeworks/week13/hw2/js/main.js
+++ homeworks/week13/hw2/js/main.js
@@ -1,40 +1,72 @@
-$(document).ready(function(){
-  $("#button-add").click(function(){
-    if ($(".form-control").val() != "") {      
+$(document).ready(() => {
+  /* 函式區 */
+  // 計算並呈現當前未完成任務數量
+  function countTasks() {
+    const items = $('.list > .uncompleted').length;
+    const text = `Here are <em class="highlight">${items}</em> pending items`;
+    $('.list__title').html(text);
+  }
+  // 計算並呈現當前完成任務數量
+  function countCompleteTasks() {
+    const items = $('.list > .completed').length;
+    const text = `You have completed <em class="highlight">${items}</em> tasks`;
+    $('.list__title--completed').html(text);
+  }
+  // 增加未完成任務清單
+  function addList() {
+    if ($('.form-control').val() !== '') {
       const listText = `
-      <div class="input-group mb-3 px-3">
-        <div class="input-group-prepend">
-          <div class="input-group-text">
-            <input type="checkbox" aria-label="Checkbox for following text input">
+        <div class="list__items input-group mb-3 uncompleted">
+          <div class="input-group-prepend  border-0">
+            <label class="mb-0"><input type="checkbox" class="list__check" aria-label="Checkbox for following text input"></label>
+          </div>
+          <div class="list__text text-secondary border-0">
+            <p class="mb-0">${$('.form-control').val()}</p>
+          </div>
+          <div class="list__close input-group-append border-0">
+            <button type="button" class="btn btn-danger close p-2" aria-label="Close">
+              <span aria-hidden="true" aria-label="Close">&times;</span>
+            </button>
           </div>
         </div>
-        <div class="list__text text-secondary">
-          <p class="mb-0">${$(".form-control").val()}</p>
-        </div>
-        <div class="input-group-append list__close">
-          <button type="button" class="btn btn-danger close p-2" aria-label="Close">
-            <span aria-hidden="true" aria-label="Close">&times;</span>
-          </button>
-        </div>
-      </div>
-      `
-      $(".list").append(listText);
-      $(".form-control").val("");
+      `;
+      $('.list__title--completed').before(listText);
+      $('.form-control').val('');
+      countTasks();
     }
-  })
+  }
+  /* 函式區 END */
 
-  $(".list").click(function(e) {
+  /* 監聽事件區 */
+  $('#button-add').click(() => {
+    addList();
+  });
+  $('.form-control').keyup((e) => {
+    if (e.key === 'Enter') addList();
+  });
+
+  $('.list').click((e) => {
     // 刪除按鈕
-    if ($(e.target).attr("aria-label") === "Close") {
-      $(e.target).parentsUntil(".list").remove();
+    if ($(e.target).attr('aria-label') === 'Close') {
+      $(e.target).parents('.list__items').remove();
+      countTasks();
+      countCompleteTasks();
     }
-
-    // 勾選完成按鈕
-    if ($(e.target).hasClass("list__checked")) {
-      $(e.target).parents(".input-group-text").toggleClass("completed");
-      $(e.target).parents(".input-group").find(".list__text").toggleClass("text-decoration-deleted");
-    };
-  })
-
-  $()  
+    // 勾選框按鈕
+    if ($(e.target).hasClass('list__check')) {
+      const list = $(e.target).parents(('.list__items'));
+      list.toggleClass('completed');
+      list.toggleClass('uncompleted');
+      if (list.hasClass('completed')) {
+        $('.list__title--completed').after(list);
+      } else {
+        $('.list__title--completed').before(list);
+      }
+      countTasks();
+      countCompleteTasks();
+    }
+  });
+  /* 監聽事件區 END */
+  countTasks();
+  countCompleteTasks();
 });
