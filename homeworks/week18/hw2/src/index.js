6a48be1088b31e367b9f98af036da4fe9d7a5a8d
Zihur <zihur@bsdctw.com>
2019-11-23 12:34:12 +0800

    WIP webpack 引入方式測試完成，待測 sourcemap 和 dev 模式

--- homeworks/week18/hw2/src/index.js
+++ homeworks/week18/hw2/src/index.js
@@ -1,9 +1,21 @@
-// 經實測，require 的寫法最終產生出來的的檔案會大一些，以此份檔案為例 968 byte
-// const utils = require('./utils.js');
+/**
+ * 經實測，require 的寫法最終產生出來的的檔案會大一些，以此份檔案為例 968 byte
+ * import from 的寫法最終產生出來的檔案會小一些，以此份檔案為例 1.04 Kib
+ * import from 方法目前也能直接被較新的瀏覽器所接受，因為被包含在了 ES6 的規範內
+ * 如果要壓榨出極限的載入效能優化，應該是個可以考慮的點 XDD
+ */
 
-// import from 的寫法最終產生出來的檔案會小一些，以此份檔案為例 1.04 Kib
-// 此方法目前也能直接被較新的瀏覽器所接受，因為被包含在了 ES6 的規範內
-// 如果要壓榨出極限的載入效能優化，應該是個可以考慮的點 XDD
+/**
+ * 在撰寫下面引入位置檔案名稱的時候
+ * 根據 eslint 和開發社群的建議，在開發時似乎不要再加入副檔名(擴展名)，讓套件自動判斷較好
+ * 但若未經打包，尚未確認瀏覽器是否能自動判斷此檔案，不知哪種為正確
+ * eslint 預設加入擴展名，會被報錯
+ */
+// const utils = require('./utils.js');
 import utils from './utils';
 
-console.log(utils(3, 6));
+/**
+ * 測試如何呼叫同隻檔案的多個 function
+*/
+console.log(utils.add(3, 6));
+console.log(utils.mutiple(6, 8));
--- homeworks/week18/hw2/src/utils.js
+++ homeworks/week18/hw2/src/utils.js
@@ -1,3 +1,15 @@
-export default function add(a, b) {
+// export default function add(a, b) {
+//   return a + b;
+// }
+
+
+// 下面為測試第二種 export 方法，此方式看起來能同時
+function add(a, b) {
   return a + b;
 }
+
+function mutiple(a, b) {
+  return a * b;
+}
+
+module.exports = { add, mutiple };
